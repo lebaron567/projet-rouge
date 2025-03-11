8798500b195622c9de6d77fba66d1b71ecb1945d
lebaron567 <emericbarotin@gmail.com>
2025-03-11 13:28:45 +0100

    replace MySQL with SQLite for database connection; implement AutoMigrate for User and Post models

--- go-api-project/go.mod
+++ go-api-project/go.mod
@@ -6,14 +6,13 @@ toolchain go1.23.2
 
 require (
 	github.com/go-chi/chi/v5 v5.2.1
-	gorm.io/driver/mysql v1.5.7
+	gorm.io/driver/sqlite v1.5.7
 	gorm.io/gorm v1.25.12
 )
 
 require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.9.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 	golang.org/x/text v0.23.0 // indirect
 )
--- go-api-project/internal/db/db.go
+++ go-api-project/internal/db/db.go
@@ -1,19 +1,24 @@
 package db
 
 import (
-	"log"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+    "log"
+    "gorm.io/driver/sqlite"
+    "gorm.io/gorm"
+    "go-api-project/internal/models"
 )
 
 var DB *gorm.DB
 
 func Init() {
-	dsn := "username:password@tcp(127.0.0.1:3306)/database_name?charset=utf8mb4&parseTime=True&loc=Local"
-	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Error connecting to the database: %v", err)
-	}
+    var err error
+    DB, err = gorm.Open(sqlite.Open("message.db"), &gorm.Config{})
+    if err != nil {
+        log.Fatalf("Error connecting to the database: %v", err)
+    }
+
+    // AutoMigrate will create the tables, missing foreign keys, constraints, columns and indexes.
+    err = DB.AutoMigrate(&models.User{}, &models.Post{})
+    if err != nil {
+        log.Fatalf("Error migrating the database: %v", err)
+    }
 }
